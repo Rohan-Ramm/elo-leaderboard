948455478d46611b158f43c303159587883253fa
Rohan <ramma006@umn.edu>
2026-08-25 16:55:06 -0700

    Write menu css

--- src/subpages/main-menu.tsx
+++ src/subpages/main-menu.tsx
@@ -6,7 +6,20 @@ function MainMenu() {
 
   return (
     <main className="container">
-      <p>Main Menu</p>
+      <div className="menu-container">
+        <h1 className="menu-title">Main Menu</h1>
+        
+        <div className="button-grid">
+            <button className="menu-btn">View Leaderboard</button>
+            <button className="menu-btn">Settings</button>
+            
+            <button className="menu-btn">Add Game (single)</button>
+            <button className="menu-btn">Add Game<br/>(multiple)</button>
+            
+            <button className="menu-btn">Import League</button>
+            <button className="menu-btn">Export League</button>
+        </div>
+    </div>
     </main>
   );
 }
